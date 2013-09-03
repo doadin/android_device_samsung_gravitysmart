89dec5491fc4d2f3fa4ce6db34b955047753df24
Doadin <tbkizle@gmail.com>
2013-09-03 14:44:04 -0400

    update/fix

--- cm.mk
+++ cm.mk
@@ -4,16 +4,16 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 #$(call inherit-product, $(LOCAL_PATH)/device_gravitysmart.mk)
 
-PRODUCT_RELEASE_NAME := ancora
+PRODUCT_RELEASE_NAME := gravitysmart
 
 #CM_EXPERIMENTAL := true
 #CM_EXTRAVERSION := alpha1
 
-# Setup device configuration
-PRODUCT_NAME := SGH-T589
-PRODUCT_DEVICE := SGH-T589
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := SGH-T589
+## Setup device configuration (Is Elsewhere)
+#PRODUCT_NAME := SGH-T589
+#PRODUCT_DEVICE := SGH-T589
+#PRODUCT_BRAND := Samsung
+#PRODUCT_MANUFACTURER := Samsung
+#PRODUCT_MODEL := SGH-T589
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/SGH-T589/SGH-T589/SGH-T589:2.2.2/FROYO/UVKF1:user/release-keys PRIVATE_BUILD_DESC="SGH-T589-user 2.2.2 FROYO UVKF1 release-keys"
